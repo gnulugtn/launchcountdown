15c704d06f8f91c9dabc35f65af700dce1d4c980
Ahmed Sghaier <ahmed@angrycactus.biz>
2015-12-25 14:39:23 +0100

    date offset fix, quick enhancements: background, composition

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,17 @@
 $(function() {
 	window.scrollReveal = new scrollReveal();
 	"use strict";
-	
+
 	// PreLoader
 	$(window).load(function() {
 		$(".loader").fadeOut(400);
 	});
 
 	// Backstretchs
-	$("#header").backstretch("images/tn.jpg");
-	$("#services").backstretch("images/3.jpg");
-	
+
 	// Countdown
 	$('.countdown').downCount({
-		date: '1/1/2016 00:00:00',
-		offset: +10
-	});			
-    
+		date: '1/1/2016 00:00:00'
+	});
+
 });
